0cc49408dc2a6400cc17ffb183a1ae353d3fb647
THEBAULT Julien <julien@thebault.co>
2019-10-16 17:43:22 +0000

    Add more and more tools

---
+++ roles/settings/templates/docker/ykperso/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+init(){
+	local pcscd_running
+	pcscd_running=$(pgrep pcscd)
+	if [ -z "$pcscd_running" ]; then
+		echo "starting pcscd in backgroud"
+		pcscd --debug --apdu
+		pcscd --hotplug
+	else
+		echo "pcscd is running in already: ${pcscd_running}"
+	fi
+}
+
+init
+
+"$@"
