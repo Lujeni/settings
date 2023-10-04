bf5669db416ea565e67b6d71e7fa2a9c7ed5418c
THEBAULT Julien <julien.thebault@numberly.com>
2023-10-04 10:06:15 +0200

    feat(nvim): support venv-selector plugin

--- roles/settings/files/nvim/lua/lujeni/plugins/neo-tree.lua
+++ roles/settings/files/nvim/lua/lujeni/plugins/neo-tree.lua
@@ -12,5 +12,10 @@ return {
   end,
   opts = {
     close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      }
+    }
   }
 }
---
+++ roles/settings/files/nvim/lua/lujeni/plugins/venv-selector.lua
@@ -0,0 +1,10 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+	opts = {},
+	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	keys = {{
+		"<leader>vs", "<cmd>:VenvSelect<cr>",
+		"<leader>vc", "<cmd>:VenvSelectCached<cr>"
+	}}
+}
