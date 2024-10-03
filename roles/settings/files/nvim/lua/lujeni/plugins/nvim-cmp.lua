ab2209319549fdc4312e6b31ebd51f859ac09488
Lujeni <julien@thebault.co>
2024-10-03 17:34:30 +0200

    feat(neovim): continue the refresh

--- roles/settings/files/nvim/lua/lujeni/plugins/nvim-cmp.lua
+++ roles/settings/files/nvim/lua/lujeni/plugins/nvim-cmp.lua
@@ -4,30 +4,11 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
-		{
-			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 	},
 	config = function()
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
 		cmp.setup({
-			view = {
-				entries = "native",
-			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
--- roles/settings/files/nvim/lua/lujeni/plugins/venv-selector.lua
+++ roles/settings/files/nvim/lua/lujeni/plugins/venv-selector.lua
@@ -1,10 +1,11 @@
 return {
 	"linux-cultist/venv-selector.nvim",
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {},
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-	keys = {{
-		"<leader>vs", "<cmd>:VenvSelect<cr>",
-		"<leader>vc", "<cmd>:VenvSelectCached<cr>"
-	}}
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	config = function()
+		require("venv-selector").setup()
+	end,
+	keys = {
+		{ ",sv", "<cmd>VenvSelect<cr>" },
+	},
 }
