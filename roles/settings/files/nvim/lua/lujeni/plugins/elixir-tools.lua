ebefef3be718e144d594d5f315a807f5bebd7e3c
Lujeni <julien@thebault.co>
2024-10-21 11:03:55 +0200

    feat(neovim): hardtime

--- roles/settings/files/nvim/lua/lujeni/plugins/elixir-tools.lua
+++ roles/settings/files/nvim/lua/lujeni/plugins/elixir-tools.lua
@@ -1,27 +1,31 @@
 return {
-  "elixir-tools/elixir-tools.nvim",
-  version = "*",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local elixir = require("elixir")
-    local elixirls = require("elixir.elixirls")
+	"elixir-tools/elixir-tools.nvim",
+	version = "*",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local elixir = require("elixir")
+		local elixirls = require("elixir.elixirls")
 
-    elixir.setup {
-      elixirls = {
-        settings = elixirls.settings {
-          dialyzerEnabled = false,
-          enableTestLenses = false,
-          suggestSpecs = true,
-        },
-        on_attach = function(client, bufnr)
-          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-        end,
-      }
-    }
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+		elixir.setup({
+			nextls = { enable = true },
+			elixirls = {
+				enable = true,
+				settings = elixirls.settings({
+					dialyzerEnabled = false,
+					enableTestLenses = false,
+				}),
+				on_attach = function(client, bufnr)
+					vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+					vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+					vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+				end,
+			},
+			projectionist = {
+				enable = true,
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
